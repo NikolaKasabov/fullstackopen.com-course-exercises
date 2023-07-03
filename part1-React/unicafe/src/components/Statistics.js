dced8f9dabc14e99f90f9915eebc6b5f31ff7a2a
NikolaKasabov <n_kasabov@mail.bg>
2023-07-03 20:15:43 +0300

    unicafe step 4 completed

--- part1-React/unicafe/src/components/Statistics.js
+++ part1-React/unicafe/src/components/Statistics.js
@@ -1,4 +1,4 @@
-function Statistics({good, neutral, bad}) {
+function Statistics({ good, neutral, bad }) {
   const all = good + neutral + bad;
   const average = (good - bad) / all;
   const positive = (good / all) * 100 + ' %';
@@ -6,12 +6,18 @@ function Statistics({good, neutral, bad}) {
   return (
     <div>
       <h3>statistics</h3>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive}</p>
+      {(good || neutral || bad)
+        ? <>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+            <p>all {all}</p>
+            <p>average {average}</p>
+            <p>positive {positive}</p>
+          </>
+        : <p>No feedback given</p>
+      }
+
     </div>
   );
 }
