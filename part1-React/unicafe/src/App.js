8c3a03d864ff37aded9f7e8e3ffa01c9fae49535
NikolaKasabov <n_kasabov@mail.bg>
2023-07-03 20:06:42 +0300

    unicafe step 2 completed

--- part1-React/unicafe/src/App.js
+++ part1-React/unicafe/src/App.js
@@ -6,6 +6,10 @@ function App() {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100 + ' %';
+
   return (
     <div>
       <h2>give feedback</h2>
@@ -16,6 +20,9 @@ function App() {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
     </div>
   );
 }
