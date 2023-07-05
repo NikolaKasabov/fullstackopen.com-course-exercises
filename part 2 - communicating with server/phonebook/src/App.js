f6a485f7b4c15cd8e182ff6ab1a23ade1ee7260b
NikolaKasabov <n_kasabov@mail.bg>
2023-07-06 01:11:26 +0300

    phonebook step 6 completed

--- part 2 - communicating with server/phonebook/src/App.js
+++ part 2 - communicating with server/phonebook/src/App.js
@@ -1,19 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
+import axios from 'axios';
+
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: nanoid() },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: nanoid() },
-    { name: 'Dan Abramov', number: '12-43-234345', id: nanoid() },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: nanoid() },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+
+  useEffect(() => { 
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      });
+  }, []);
 
   function isNameAlreadyAdded(name) {
     return persons.find(person => person.name === name);
