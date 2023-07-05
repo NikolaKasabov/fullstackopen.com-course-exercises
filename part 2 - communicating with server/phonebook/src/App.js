87365b4d0e9a8d049e9d4a1f1f5f4cc21c9226e7
NikolaKasabov <n_kasabov@mail.bg>
2023-07-05 20:07:53 +0300

    phonebook step 4 completed

--- part 2 - communicating with server/phonebook/src/App.js
+++ part 2 - communicating with server/phonebook/src/App.js
@@ -3,14 +3,15 @@ import { nanoid } from 'nanoid';
 
 function App() {
   const [persons, setPersons] = useState([
-    {
-      id: nanoid(),
-      name: 'Arto Hellas',
-      number: '040-1234567',
-    },
+    { name: 'Arto Hellas', number: '040-123456', id: nanoid() },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: nanoid() },
+    { name: 'Dan Abramov', number: '12-43-234345', id: nanoid() },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: nanoid() },
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+  const [filteredPersons, setFilteredPersons] = useState(persons);
 
   function isNameAlreadyAdded(name) {
     return persons.find(person => person.name === name);
@@ -38,10 +39,23 @@ function App() {
     setNewNumber('');
   }
 
+  function handleFilter(ev) {
+    const filterValue = ev.target.value;
+    setFilter(filterValue);
+    const filtered = persons.filter(person => person.name.toLowerCase().includes(filterValue.toLowerCase()));
+    setFilteredPersons(filtered);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        filter shown with <input type='text' value={filter} onChange={handleFilter} />
+      </div>
+
       <form onSubmit={handleSubmit}>
+        <h3>add a new</h3>
         <div>
           name: <input type='text' value={newName} onChange={ev => setNewName(ev.target.value)} />
         </div>
@@ -50,8 +64,9 @@ function App() {
         </div>
         <div><button type="submit">add</button></div>
       </form>
+
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.id}>{person.name} {person.number}</p>)}
+      {filteredPersons.map(person => <p key={person.id}>{person.name} {person.number}</p>)}
     </div>
   )
 }
