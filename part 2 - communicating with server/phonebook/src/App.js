af0e5b0533db103869cabd4d947c3f2325526f17
NikolaKasabov <n_kasabov@mail.bg>
2023-07-05 19:53:02 +0300

    phonebook step 3 completed

--- part 2 - communicating with server/phonebook/src/App.js
+++ part 2 - communicating with server/phonebook/src/App.js
@@ -6,9 +6,11 @@ function App() {
     {
       id: nanoid(),
       name: 'Arto Hellas',
+      number: '040-1234567',
     },
   ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   function isNameAlreadyAdded(name) {
     return persons.find(person => person.name === name);
@@ -16,8 +18,6 @@ function App() {
 
   function handleSubmit(ev) {
     ev.preventDefault();
-
-    if (!newName) return;
 
     if (isNameAlreadyAdded(newName)) {
       alert(`${newName} is already added to the phonebook.`);
@@ -28,12 +28,14 @@ function App() {
       ...persons,
       {
         id: nanoid(),
-        name: newName
+        name: newName,
+        number: newNumber,
       },
     ];
 
     setPersons(newPersons);
     setNewName('');
+    setNewNumber('');
   }
 
   return (
@@ -41,14 +43,15 @@ function App() {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={ev => setNewName(ev.target.value)} />
+          name: <input type='text' value={newName} onChange={ev => setNewName(ev.target.value)} />
         </div>
         <div>
-          <button type="submit">add</button>
+          number: <input type='text' value={newNumber} onChange={ev => setNewNumber(ev.target.value)} />
         </div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.id}>{person.name}</p>)}
+      {persons.map(person => <p key={person.id}>{person.name} {person.number}</p>)}
     </div>
   )
 }
